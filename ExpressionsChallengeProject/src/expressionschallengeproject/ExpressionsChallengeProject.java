c47ae889d762f3608bbaf3c8924772c325c134dd
Joaovitron999 <jv.fonseca999@gmail.com>
2022-11-10 16:44:40 -0300

    inicial commit

---
+++ ExpressionsChallengeProject/src/expressionschallengeproject/ExpressionsChallengeProject.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expressionschallengeproject;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 2020.1.08.003
+ */
+public class ExpressionsChallengeProject {
+
+
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int n, d;
+        n = input.nextInt();
+        d = input.nextInt();
+
+        
+    }
+    
+}
